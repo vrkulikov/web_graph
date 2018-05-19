ab0c16e516c375b2596f1d0508c2fa79167eb4eb
vladimir <v.r.kulikov@mail.ru>
2018-05-19 18:30:25 +0700

    add diameter

--- networkit_test.py
+++ networkit_test.py
@@ -1,3 +1,4 @@
 import networkit as nk
-g = nk.generators.ErdosRenyiGenerator(1000,0.8).generate()
+g = nk.generators.ErdosRenyiGenerator(1000,0.05).generate()
 nk.overview(g)
+print(nk.distance.Diameter(g).getDiameter())
